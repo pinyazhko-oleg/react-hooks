a99f8b51e03d6a93ee7e55f22d7a7f33caa5cf2d
Pinia <pinyazchko.oleg@gmail.com>
2020-06-30 13:51:24 +0300

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,44 +1,53 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import TodoList from './TodoList'
 
-const App =  () => {
+const App = () => {
 
-  const [todos, setTodos] = useState([
-    {id: 1, title: 'First todo', completed: false},
-    {id: 2, title: 'Second todo', completed: false},
-  ])
+    const [todos, setTodos] = useState([])
 
-  const [todoTitle, setTodoTitle] = useState('')
+    const [todoTitle, setTodoTitle] = useState('')
 
-  const addTodo = (e) => {
-    if (e.key === 'Enter') {
-      setTodos([
-          ...todos,
-        {
-          id: Date.now(),
-          title: todoTitle,
-          completed: false
+    const handleClick = () => console.log('click')
+
+    useEffect(()=>{
+        const raw = localStorage.getItem('todos') || []
+        setTodos(JSON.parse(raw))
+        document.removeEventListener('click', handleClick)
+    }, [])
+
+    useEffect(() => {
+        localStorage.setItem('todos', JSON.stringify(todos))
+    }, [todos])
+
+    const addTodo = (e) => {
+        if (e.key === 'Enter') {
+            setTodos([
+                ...todos,
+                {
+                    id: Date.now(),
+                    title: todoTitle,
+                    completed: false
+                }
+            ])
+            setTodoTitle('')
         }
-      ])
-      setTodoTitle('')
     }
-  }
 
     return (
-      <div className="container">
-        <h1>Todo app</h1>
+        <div className="container">
+            <h1>Todo app</h1>
 
-          <div className="input-field">
-            <input type="text"
-                   value={todoTitle}
-                   onChange={e => setTodoTitle(e.target.value)}
-                   onKeyPress={addTodo}
-            />
-            <label>Todo name</label>
-          </div>
+            <div className="input-field">
+                <input type="text"
+                       value={todoTitle}
+                       onChange={e => setTodoTitle(e.target.value)}
+                       onKeyPress={addTodo}
+                />
+                <label>Todo name</label>
+            </div>
 
-          <TodoList todos={todos} />
-      </div>
+            <TodoList todos={todos}/>
+        </div>
     );
 }
 
